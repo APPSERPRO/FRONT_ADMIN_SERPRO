36766a48de3b94a94ac5775197704f3d459ad077
Caivan <caivan2@gmail.com>
2020-09-15 15:45:19 -0500

    Configuration NGPrime and Quill

--- AdminSerPro/src/app/app.module.ts
+++ AdminSerPro/src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionCreationComponent } from './serpro-components/question-creation/question-creation.component';
+import { MultipleSelctionQuestionComponent } from './serpro-components/question-creation/question/multiple-selction-question/multiple-selction-question.component';
+
 
 // Import NGPrime Modules
 import {EditorModule} from 'primeng/editor';
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionCreationComponent
+    QuestionCreationComponent,
+    MultipleSelctionQuestionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    EditorModule
+    EditorModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

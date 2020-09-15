715c8affc53529aecbd06c553edd18b7a0fa27d1
Caivan <caivan2@gmail.com>
2020-09-15 16:51:21 -0500

    Change the array of answer from string to Answer class

--- AdminSerPro/src/app/models/question.model.ts
+++ AdminSerPro/src/app/models/question.model.ts
@@ -1,26 +1,28 @@
+import { Answer } from './answer.model';
+
 export class Question {
 
     statement: string;
     type: string;
-    answers: string [];
+    answers: Answer [];
     images: any;
     selectedAnswer: string;
     feedback : string;
     correctAnswer : string;
 
-    constructor (statement: string, answers: string [], type: string, images: any, correctAnswer:string, feedback:string){
+    constructor (statement?: string, answers?: Answer [], type?: string, images?: any, correctAnswer?:string, feedback?:string) {
         this.statement = statement;
-        this.answers = answers;
+        if (answers) {
+            this.answers = answers;
+        } else {
+            this.answers = [];
+        }
+        
         this.type = type;
         this.images = images;
         this.selectedAnswer = null;
         this.correctAnswer = correctAnswer;
         this.feedback = feedback;
     }
-
-    checkAnswer(): boolean{
-    return this.selectedAnswer === this.correctAnswer;
-    }
-
 
 }

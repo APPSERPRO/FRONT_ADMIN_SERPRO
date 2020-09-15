33ab4ca41698ed8c90ec3db0a5c7d12383c97acb
Caivan <caivan2@gmail.com>
2020-09-15 15:10:15 -0500

    create component multiple selection question

---
+++ AdminSerPro/src/app/serpro-components/question-creation/question/multiple-selction-question/multiple-selction-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-selction-question',
+  templateUrl: './multiple-selction-question.component.html',
+  styleUrls: ['./multiple-selction-question.component.css']
+})
+export class MultipleSelctionQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

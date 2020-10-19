e194afa591886cabbd44bfa0833d07ad82ca3076
NicolasSoler <josolerh@poligran.edu.co>
2020-10-19 03:56:34 -0500

    Confirmacion de espacios vacios Modulo

--- src/app/serpro-components/module-creation/module-creation.component.ts
+++ src/app/serpro-components/module-creation/module-creation.component.ts
@@ -15,10 +15,12 @@ export class ModuleComponent implements OnInit {
   items: SelectItem[];
   item: string;
   display: boolean;
+  mensaje: String;
 
   constructor(private icfesModuleService: IcfestModuleService) {
+    this.display=false;
     this.icfesModule = new IcfesModule();
-
+    this.mensaje = "Algo falta por llenar, \n Por favor revise los campos que ningunno se encuentre vacio";
    }
 
   ngOnInit(): void {
@@ -30,14 +32,24 @@ export class ModuleComponent implements OnInit {
   }
 
   saveIcfesModule(){
-    if(this.icfesModule.description != null){
-      if(this.icfesModule.evaluationSubject != null){
-        if(this.icfesModule.knowledgeArea != null){
-          this.icfesModuleService.posIcfesModule(this.icfesModule);
+   try{
+      if(this.icfesModule.description != null){
+        if(this.icfesModule.evaluationSubject != null){
+          if(this.icfesModule.knowledgeArea != null){
+            this.icfesModuleService.posIcfesModule(this.icfesModule);
+            this.mensaje = "El modulo se ha subido correctamente";
+            this.showDialog();
+          }else {
+            this.showDialog();
+          }
+        }else {
           this.showDialog();
         }
+      }else {
+        this.showDialog();
       }
-    }
+   }catch(err){
+    this.showDialog();
+   }
   }
-
 }
